c2bc80d1b2ea6d51b39cca1ec138022ff862935f
hhindley <s2257179@ed.ac.uk>
2022-11-16 18:00:02 +0000

    new functions for param analysis and real data bopt

--- rtc_models/Oct2022_model/rtc_model_analysis_toy_params.jl
+++ rtc_models/Oct2022_model/rtc_model_analysis_toy_params.jl
@@ -8,35 +8,24 @@ L = 100; c = 0.01; kr = 10; Vmax_init = 5; Km_init = 55.829; ω_r = 4.14; ω_ab 
 d = 0.01; krep = 10;  kdam = 0.05; ktag = 10; kdeg = 0.001; kin = 0.4; atp = 10;
 rtca_0 = 1; rtcb_0 = 1;
 
+params = @SVector [L, c, kr, Vmax_init, Km_init, ω_ab, ω_r, θtscr, g_max, θtlr, km, k_b, gr_c, d, krep, kdam, ktag, kdeg, kin, atp]
+
 solu = sol(rtc_model, init, tspan, params)
+
 Plots.plot(solu[2:end], ylabel="[species]", labels=["rm_a" "rtca" "rm_b" "rtcb" "rm_r" "rtcr" "rh" "rd" "rt"], yaxis=(:log10, (1,Inf)), xaxis=(:log10, (1,Inf)))
 
-for (i,j) in zip(results, species)
-    push!(i, get_curve(solu, j))
-end
-
-dict_res = OrderedDict("rm_a"=>[], "rtca"=>[], "rm_b"=>[], "rtcb"=>[], "rm_r"=>[], "rtcr"=>[], "rh"=>[], "rd"=>[], "rt"=>[])
-
-results[1]
-
-Plots.plot(solu.t, [results[2],results[4],results[6]], xaxis=(:log10, (0.01,Inf)), labels=["RtcA" "RtcB" "RtcR"])
-Plots.plot(solu.t, [rm_a, rm_b, rm_r], xaxis=(:log10, (0.01,Inf)), labels=["rm_a" "rm_b" "rm_r"])
-Plots.plot(solu.t, [rt,rh,rd], xaxis=(:log10, (0.01,Inf)), labels=["Rt" "Rh" "Rd"])
+dict_res = get_all_curves(solu, all_species)
 
 
+Plots.plot(solu.t, [dict_res["rtca"],dict_res["rtcb"],dict_res["rtcr"]], xaxis=(:log10, (0.01,Inf)), labels=["RtcA" "RtcB" "RtcR"])
+Plots.plot(solu.t, [dict_res["rm_a"], dict_res["rm_b"], dict_res["rm_r"]], xaxis=(:log10, (0.01,Inf)), labels=["rm_a" "rm_b" "rm_r"])
+Plots.plot(solu.t, [dict_res["rh"], dict_res["rt"], dict_res["rd"]], xaxis=(:log10, (0.01,Inf)), labels=["Rt" "Rh" "Rd"])
 
-
-rm_a = get_curve(solu, :rm_a); rm_b = get_curve(solu, :rm_b); rm_r = get_curve(solu, :rm_r)
-rtca = get_curve(solu, :rtca); rtcb = get_curve(solu, :rtcb); rtcr = get_curve(solu, :rtcr)
-rh = get_curve(solu, :rh); rd = get_curve(solu, :rd); rt = get_curve(solu, :rt)
 
 tlr_el = g_max*atp/(θtlr+atp)
 tlr = rh[end]*rm_a[end]*tlr_el
 
-Plots.plot(solu.t, [rtca,rtcb,rtcr], xaxis=(:log10, (0.01,Inf)), labels=["RtcA" "RtcB" "RtcR"])
-Plots.plot(solu.t, [rm_a, rm_b, rm_r], xaxis=(:log10, (0.01,Inf)), labels=["rm_a" "rm_b" "rm_r"])
-Plots.plot(solu.t, [rt,rh,rd], xaxis=(:log10, (0.01,Inf)), labels=["Rt" "Rh" "Rd"])
-
+#need changing
 r_tot = rh+rt+rd
 Plots.plot(solu.t, [(@.rh/r_tot *100), (@.rt/r_tot *100), (@.rd/r_tot *100)], xaxis=(:log10, (0.01,Inf)), labels=["Rt" "Rh" "Rd"])
 
