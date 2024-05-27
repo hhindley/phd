81a80d1c04d251cf836db39e43257ac9ecc131ff
hhindley <s2257179@ed.ac.uk>
2024-05-27 17:29:32 +0100

    thresh status file

---
+++ stochastic_hybrid_code/thresh_analysis.jl
@@ -0,0 +1,12 @@
+using StatsBase, Distributions, Random, DataFrames, CSV, PlotlyJS, DifferentialEquations, OrderedCollections, ProgressBars, BenchmarkTools, Statistics
+
+include("/home/hollie_hindley/Documents/phd/stochastic_hybrid_code/analysis_funcs.jl")
+
+threshold_vals = range(10,310,length=20)
+df_times = DataFrame(CSV.File("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_times.csv")) # times for kdam_test1
+
+df_times
+plot(scatter(x=df_times.threshold, y=df_times.time/60/60))
+
+
+dfs = [DataFrame(CSV.File(joinpath("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_test", file), header=["event", "time", "rm_a", "rtca", "rm_b", "rtcb", "rm_r", "rtcr", "rh", "rd", "rt", "volume", "totprop"])) for file in readdir("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_test")]
