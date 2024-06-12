fe04d4b84e9ecd509df13844f44a0959c1274b25
hhindley <s2257179@ed.ac.uk>
2024-06-12 17:05:42 +0100

    new sorting function to increase speed, may need more work

--- stochastic_hybrid_code/setup/file_funcs.jl
+++ stochastic_hybrid_code/setup/file_funcs.jl
@@ -14,13 +14,23 @@ function arrow_conv(folder_path, arrow_folder_path)
     rm(folder_path, recursive=true, force=true)
 end
 
-
+function get_prop_cols(df)
+    return @view df[findall(row -> length(row[:event]) > 3, eachrow(df)), :]
+end
+function str_to_arr(str)
+    str = replace(str, "[" => "", "]" => "", " " => "")
+    str_arr = split(str, ",")
+    return parse.(Float64, str_arr)
+end
+function get_reacts(df)
+    return @view df[findall(row -> length(row[:event]) < 3 && row[:event][1] != 0, eachrow(df)), :]
+end
 
 function loadandsort_arrow_file(file)
     df = Arrow.Table(file) |> DataFrame
 
-    df_p = filter(row -> length(row[:event]) > 3, df)
-    df_r = filter(row -> length(row[:event]) < 3 && row[:event][1] != 0, df)
+    df_p = df[findall(row -> length(row[:event]) > 3, eachrow(df)), :]
+    df_r = df[findall(row -> length(row[:event]) < 3 && row[:event][1] != 0, eachrow(df)), :]
 
     react_names = [:tscr_ab, :tscr_r, :tlr_a, :tlr_b, :tlr_r, :Vinflux, :Vdam, :Vtag, :Vrep, :deg_rd, :deg_rma, :deg_rmb, :deg_rmr, :V]
 
@@ -37,9 +47,30 @@ function loadandsort_arrow_file(file)
     return df_prop, df_react, df_results
 end
 
+function new_func_arrow(file)
+    df = Arrow.Table(file) |> DataFrame
+
+    df_r = get_reacts(df)
+
+    df_p = get_prop_cols(df)
+    
+    react_names = [:tscr_ab, :tscr_r, :tlr_a, :tlr_b, :tlr_r, :Vinflux, :Vdam, :Vtag, :Vrep, :deg_rd, :deg_rma, :deg_rmb, :deg_rmr, :V]
+    
+    props = str_to_arr.(df_p.event)
+    
+    df_prop = DataFrame(transpose(hcat(props...)), react_names)
+        
+    df_r.event = [parse.(Float64, subarray) for subarray in df_r.event]
+    df_react = combine(groupby(df_r, :event), nrow => :count)[2:12,:]
+    insertcols!(df_react, :reaction => react_names[Int64.(df_react.event)])
+    
+    df_results = select!(df_p, Not(:event))
+
+return df_prop, df_react, df_results
+end
 
 function loadsort_all_arrow_files(folder_path)
-    files = readdir(folder_path)[1:5]
+    files = readdir(folder_path)[1:2]
     n = length(files)
     df_props = Vector{DataFrame}(undef, n)
     df_reacts = Vector{DataFrame}(undef, n)
--- stochastic_hybrid_code/thresh_analysis.jl
+++ stochastic_hybrid_code/thresh_analysis.jl
@@ -1,98 +1,103 @@
 using StatsBase, Distributions, Random, DataFrames, CSV, DifferentialEquations, OrderedCollections, ProgressBars, BenchmarkTools, Statistics, Arrow, FilePathsBase, Distributed
-using PlotlyJS
-# using InteractiveViz, WGLMakie
+# using PlotlyJS
+using InteractiveViz, GLMakie
 
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/analysis_funcs.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/file_funcs.jl"))
 
-threshold_vals = range(10,310,length=20)
+@elapsed threshold_vals = range(10,310,length=20)
 df_times = DataFrame(CSV.File(joinpath(homedir(), "Documents/stochastic_hybrid/thresh_times.csv"))) # times for kdam_test1
 
 # scatter(rand(4))
 # ilines(threshold_vals, df_times.time/60/60)
 plot(scatter(x=df_times.threshold, y=df_times.time/60/60), Layout(xaxis_title="threshold", yaxis_title="time (hours)"))
 
+loadandsort_arrow_file("/Users/s2257179/stoch_files/thresh_test_arrow/")
 
-df_props, df_reacts, df_res = loadsort_all_arrow_files("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_test_arrow")
+df_props, df_reacts, df_res = loadsort_all_arrow_files("/Users/s2257179/stoch_files/thresh_test_arrow/")
 
-# df_props
-
-# @elapsed props, df_rs, df_ps = df_sort_all(dfs)
-
-# plot(scatter(x=df_ps[1].time, y=df_ps[1].rtca))
+ilines(threshold_vals, df_res[1].rm_a)
 
 
-# df_props, df_r, df_p = df_sort(dfs[1])
+@elapsed df_prop, df_react, df_results = loadandsort_arrow_file("/Users/s2257179/stoch_files/thresh_test_arrow/thresh_104.73684210526316.arrow")
 
-# df_p = filter!(row -> length(row[:event]) > 1, df)
-
-# df = Arrow.Table("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_test_arrow/thresh_10.0.arrow") |> DataFrame
-# ls = [length(i) for i in df.event]
-# unique(ls)
-# if 100 in ls
-#     println("yes")
-# end
-# sort(unique(ls))
-
-# length(df.event)
-# df_p = Arrow.Table("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_test_arrow/thresh_10.0.arrow") |> DataFrame |> filter(row -> length(row[:event]) > 3)
-# df_r = Arrow.Table("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_test_arrow/thresh_10.0.arrow") |> DataFrame |> filter(row -> length(row[:event]) < 3 && row[:event][1] != 0)
-
-# props = split.(replace.(df_p.event, r"[\[\]\(Any)]" => ""), ",")
-# props = permutedims(mapcols(x -> parse.(Float64, x), DataFrame(props, :auto)))
-# rename!(props, react_names)
-
-
-# df_p = select(df_p, Not(:event))
-# df_r.event = [parse.(Float64, subarray) for subarray in df_r.event]
+@elapsed df_prop1, df_react1, df_results1 = new_func_arrow("/Users/s2257179/stoch_files/thresh_test_arrow/thresh_104.73684210526316.arrow")
 
 
 
-# files = readdir("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_test_arrow")[1:5]
-# files = files[1:5]
-# @distributed for (i, file) in collect(enumerate(files))
-#     println(i, file)
-# end
+@elapsed df = Arrow.Table("/Users/s2257179/stoch_files/thresh_test_arrow/thresh_10.0.arrow") |> DataFrame
+
+df_r = get_reacts(df)
+df_r.event = [parse.(Float64, subarray) for subarray in df_r.event]
+df_react = combine(groupby(df_r, :event), nrow => :count)[2:12,:]
 
 
-# props = df_p.event
-# props = [split(replace(i, r"[\[\]\(Any)]" => ""), ",") for i in props]
-# props = [parse.(Float64, subarray) for subarray in props]
+@elapsed df_r = get_reacts(df)
+@elapsed df_p = get_prop_cols(df)
+
+react_names = [:tscr_ab, :tscr_r, :tlr_a, :tlr_b, :tlr_r, :Vinflux, :Vdam, :Vtag, :Vrep, :deg_rd, :deg_rma, :deg_rmb, :deg_rmr, :V]
+
+@elapsed props = str_to_arr.(df_p.event)
+
+@elapsed df_prop = DataFrame(transpose(hcat(props...)), react_names)
+
+@elapsed df_results = select!(df_p, Not(:event))
 
 
-# df_props = DataFrame([name => Vector{Float64}(undef, length(props)) for name in react_names])
+@elapsed df_r.event = [parse.(Float64, subarray) for subarray in df_r.event]
 
-# for (i, prop) in enumerate(props)
-#     df_props[i, :] = prop
-# end
-
-# df_props.V
-
-
-# df_r
-
-
-# df_react = combine(groupby(df_r, :event), nrow => :count)[2:12,:]
-# insertcols!(df_react, :reaction => react_names[Int64.(df_react.event)])
+@elapsed df_react = combine(groupby(df_r, :event), nrow => :count)[2:12,:]
+insertcols!(df_react, :reaction => react_names[Int64.(df_react.event)])
 
 
 
 
 
 
-# @elapsed df_prop, df_react, df_res = loadandsort_arrow_file("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_test_arrow/thresh_10.0.arrow")
+df_prop
+df_react
+
+df_results
+
+plotBIG(df_results.time, df_results.rm_a)
 
 
 
-# df1, df2 = Arrow.Table("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_test_arrow/thresh_10.0.arrow") |> DataFrame |> filter(row -> length(row[:event]) > 3)
-# df_r = Arrow.Table("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_test_arrow/thresh_10.0.arrow") |> DataFrame |> filter(row -> length(row[:event]) < 3 && row[:event][1] != 0)
-
-# df1
 
 
-# df2
 
-# df = Arrow.Table("/home/hollie_hindley/Documents/stochastic_hybrid/thresh_test_arrow/thresh_10.0.arrow") |> DataFrame
 
-# df1 = filter(row -> length(row[:event]) > 3, df)
-# df2 = filter(row -> length(row[:event]) < 3 && row[:event][1] != 0, df)
+
+
+@elapsed df_p = df[findall(row -> length(row[:event]) > 3, eachrow(df)), :]
+
+@elapsed df_r = df[findall(row -> length(row[:event]) < 3 && row[:event][1] != 0, eachrow(df)), :]
+
+react_names = [:tscr_ab, :tscr_r, :tlr_a, :tlr_b, :tlr_r, :Vinflux, :Vdam, :Vtag, :Vrep, :deg_rd, :deg_rma, :deg_rmb, :deg_rmr, :V]
+
+@elapsed rep = replace.(df_p.event, r"[\[\]\(Any)]" => "")
+@elapsed df_prop = split.(rep, ",")
+
+
+# @elapsed df_prop = split.(replace.(df_p.event, r"[\[\]\(Any)]" => ""), ",")
+@elapsed df_prop = permutedims(mapcols(x -> parse.(Float64, x), DataFrame(df_prop, :auto)))
+rename!(df_prop, react_names)
+
+df_results = select(df_p, Not(:event))
+df_r.event = [parse.(Float64, subarray) for subarray in df_r.event]
+
+df_react = combine(groupby(df_r, :event), nrow => :count)[2:12,:]
+insertcols!(df_react, :reaction => react_names[Int64.(df_react.event)])
+
+a = df_results.time
+a = range(minimum(df_results.time), maximum(df_results.time), length=length(df_results.time))
+ilines(a, df_results.rm_a)
+
+
+function plotBIG(x, y)
+    xvals = range(minimum(x), maximum(x), length=length(x))
+    return ilines(xvals, y)
+end
+
+
+
