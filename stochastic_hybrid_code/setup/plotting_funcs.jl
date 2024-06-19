d47740a643f8f8fcb382ea3629b0cdfb2e2931ab
hhindley <s2257179@ed.ac.uk>
2024-06-19 16:56:25 +0100

    plotting

--- stochastic_hybrid_code/setup/plotting_funcs.jl
+++ stochastic_hybrid_code/setup/plotting_funcs.jl
@@ -9,18 +9,61 @@ function plotBIG(x, y; xtitle="", ytitle="", title="")
 end
 
 
-function plot_subplots(data, species, threshold_vals)
+function plot_subplots(df_results, species, threshold_vals)
 
-    f = Figure()
-    axess=[]
-
-    for i in 1:5
-        ax = Axis(f[i, 1], xlabel = "time", ylabel = "rm_a", title="threshold: $(threshold_vals[i])")
-        push!(axess, ax)
+    f = Figure(size=(1450, 900))
+    total_rows = 5
+    total_columns = 4
+    
+    for j in 1:total_columns
+        for i in 1:total_rows
+            data_ind = i + total_rows * (j - 1)
+            println(data_ind)
+            ax = Axis(f[i, j], xlabel = "time", ylabel = "$species", title="threshold: $(round(threshold_vals[data_ind], digits=2))")
+            ilines!(f[i,j], makiex(df_results[data_ind].time), df_results[data_ind][:,species])
+    
+            # Hide x-axis decorations for axes not in the bottom row
+            if i != total_rows
+                hidexdecorations!(ax, grid=false)
+            end
+    
+            # Hide y-axis decorations for axes not in the first column
+            if j > 1
+                hideydecorations!(ax, grid=false)
+            end
+        end
     end
+    save("/Users/s2257179/phd/stochastic_hybrid_code/thresh_plots/$species.png", f)
 
-    [hidexdecorations!(ax) for ax in axess[1:end-1]]
-    [ilines!(f[i,1], makiex(data[i].time), data[i][:,species]) for i in 1:5]
+    return f
+end
+
+function plot_subplots_hists(df_reacts, threshold_vals)
+
+    f = Figure(size=(1450, 900))
+    total_rows = 5
+    total_columns = 4
+    for j in 1:total_columns
+        for i in 1:total_rows
+            data_ind = i + total_rows * (j - 1)
+            println(data_ind)
+            ax = Axis(f[i,j], ylabel="reaction count", xticks=(1:length(df_reacts[1].event), string.(collect(df_reacts[1].reaction))),xticklabelrotation=45, title= title="threshold: $(round(threshold_vals[data_ind], digits=2))")#, yscale=log10)
+            barplot!(ax, 1:length(collect(df_reacts[data_ind].event)), df_reacts[data_ind].count)
+            # ylims!(ax, 0, 1000000)
+            # Hide x-axis decorations for axes not in the bottom row
+            if i != total_rows
+                hidexdecorations!(ax, grid=false)
+            end
+    
+            # Hide y-axis decorations for axes not in the first column
+            # if j > 1
+            #     hideydecorations!(ax, grid=false)
+            # end
+            
+        end
+    end
+    linkaxes!(filter(x -> x isa Axis, f.content)...)
+    save("/Users/s2257179/phd/stochastic_hybrid_code/thresh_plots/stoch_reacts.png", f)
 
     return f
 end
--- stochastic_hybrid_code/thresh_analysis.jl
+++ stochastic_hybrid_code/thresh_analysis.jl
@@ -9,75 +9,67 @@ include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/plotting_funcs.jl"
 df_results = load_files("/Users/s2257179/stoch_files/thresh_test_arrow_files_14_06/results")
 df_props = load_files("/Users/s2257179/stoch_files/thresh_test_arrow_files_14_06/props")
 df_reacts = load_files("/Users/s2257179/stoch_files/thresh_test_arrow_files_14_06/reacts")
+df_reacts = [DataFrame(df_reacts[i]) for i in eachindex(df_reacts)]
+react_names=[:tscr_ab, :tscr_r, :tlr_a, :tlr_b, :tlr_r, :Vinflux, :Vdam, :Vtag, :Vrep, :deg_rd, :deg_rma, :deg_rmb, :deg_rmr, :V]
+for i in eachindex(df_reacts)
+    for num in 1:13
+        if num ∉ df_reacts[i].event
+            push!(df_reacts[i], (event=num, count=0, reaction=react_names[num]))
+        end
+    end
+end
+
+# for i in eachindex(df_reacts)
+#     df_reacts[i].count .+= 1e-3  # Adjust counts for each dataframe
+# end
 
 threshold_vals0 = range(10,310,length=20)
 threshold_vals1 = range(246,310,length=5)
 threshold_vals = [threshold_vals0[1:15]; threshold_vals1]
 
-times2 = DataFrame(CSV.File("/Users/s2257179/stoch_files/thresh_times_last5.csv"))
+# times2 = DataFrame(CSV.File("/Users/s2257179/stoch_files/thresh_times_last5.csv"))
 
-plotBIG(times2.threshold, times2.time/60/60, "threshold", "time (hours)")
-
-for i in 1:length(df_results)
-    display(GLMakie.Screen(), plotBIG(df_results[i].time, df_results[i].rm_a, xtitle="time", ytitle="rm_a", title="threshold = $(threshold_vals[i])"))
-end
-
-plotBIG(df_results[1].time, df_results[1].rtca, xtitle="time", ytitle="rtca")
-# display(GLMakie.Screen(), plotBIG(df_results[1].time, df_results[1].rm_a, xtitle="time", ytitle="rtca"))
-
-df_results[1]
+# plotBIG(times2.threshold, times2.time/60/60, "threshold", "time (hours)")
 
 plot_subplots(df_results, :rm_a, threshold_vals)
+plot_subplots(df_results, :rm_b, threshold_vals)
+plot_subplots(df_results, :rm_r, threshold_vals)
+
+plot_subplots(df_results, :rtca, threshold_vals)
+plot_subplots(df_results, :rtcb, threshold_vals)
+plot_subplots(df_results, :rtcr, threshold_vals)
+
+plot_subplots(df_results, :rh, threshold_vals)
+plot_subplots(df_results, :rd, threshold_vals)
+plot_subplots(df_results, :rt, threshold_vals)
+
+
+
+using Colors
+plotBIG(df_results[1].time, df_results[1].rtca)
+
+df_reacts[1]
 
 f = Figure()
-for i in 1:length(df_results)
-    xvals = range(minimum(data[i].time), maximum(data[i].time), length=length(data[i].time))
-    ax = Axis(f[1, i], xlabel = "time", ylabel = "$species", title="$(threshold_vals[i])")
-    ilines!(f[1,i], xvals, data[i][:,species])
+ax = Axis(f[1,1],yscale=log10)
+barplot!(ax, 1:length(collect(df_reacts[11].event)), df_reacts[11].count)
+# ylims!(ax, 1, 10000000)
+
+f = Figure()
+ax = Axis(f[1,1],yscale=log10)
+barplot!(ax, 1:length(collect(df_reacts[1].event)), df_reacts[1].count)
+
+df_reacts[11]
+
+
+plot_subplots_hists(df_reacts, threshold_vals)
+
+tot_counts = Int64[]
+for i in eachindex(df_reacts)
+    push!(tot_counts,sum(df_reacts[i].count))
 end
 
-f = Figure()
-xvals = range(minimum(df_results[1].time), maximum(df_results[1].time), length=length(df_results[1].time))
-ax1 = Axis(f[1, 1], xlabel = "", ylabel = "", title="")
-ax2 = Axis(f[2,1], xlabel = "", ylabel = "", title="")
-hidexdecorations!(ax1)
-linkaxes!(ax1, ax2)
-
-ilines!(f[1,1], xvals, df_results[1][:,:rm_a])
-ilines!(f[2,1], range(minimum(df_results[2].time), maximum(df_results[2].time), length=length(df_results[2].time)), df_results[2][:,:rm_a])
-
-plotBIG(df_results[1].time, df_results[1][:,:rm_a])
-
-
 
 f = Figure()
-total_rows = 5
-total_columns = 4
-
-for j in 1:total_columns
-    for i in 1:total_rows
-        data_ind = i + total_rows * (j - 1)
-        println(data_ind)
-        ax = Axis(f[i, j], xlabel = "time", ylabel = "rm_a", title="threshold: $(threshold_vals[data_ind])")
-        ilines!(f[i,j], makiex(df_results[data_ind].time), df_results[data_ind][:,:rm_a])
-
-        # Hide x-axis decorations for axes not in the bottom row
-        if i != total_rows
-            hidexdecorations!(ax, grid=false)
-        end
-
-        # Hide y-axis decorations for axes not in the first column
-        if j > 1
-            hideydecorations!(ax, grid=false)
-        end
-    end
-end
-
-# delete the last threshold results from the source data 
-
-
-plot_subplots(df_results, :rm_a, threshold_vals)
-
-
-
-
+ax = Axis(f[1,1],xlabel="threshold", ylabel="total stochastic reaction count")
+barplot!(threshold_vals, tot_counts)
