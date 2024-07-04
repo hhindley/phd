1e62ef8f5a7dcb61e8ba8785a7f206734dc8a3c3
hhindley <s2257179@ed.ac.uk>
2024-07-04 14:42:15 +0100

    histograms complete

--- stochastic_hybrid_code/threshold_analysis/histograms/make_hists.jl
+++ stochastic_hybrid_code/threshold_analysis/histograms/make_hists.jl
@@ -5,47 +5,55 @@ include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/file_funcs.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/plotting_funcs.jl"))
 
 
-function get_time_in_state(grouped_df)
-    in_sequence = false
-    stops = Int[]
-    starts = Int[]
-    index_length = length(grouped_df.actual_index)
-    # println("looping through indexes")
-    for i in 1:index_length - 1
-        current_index = grouped_df.actual_index[i]
-        next_index = grouped_df.actual_index[i + 1]
-        # println("checking if increasing by 1")
-        if current_index + 1 == next_index
-            if !in_sequence
-                # Sequence starts increasing by 1
-                in_sequence = true
-                # println("storing the start index")
-                push!(starts, i)  # Store the start index
-            end
-        else
-            if in_sequence
-                # println("sequence stops increasing by 1")
-                # Sequence stops increasing by 1
-                in_sequence = false
-                # println("storing the stop index")
-                push!(stops, i)  # Store the stop index
-            end
-        end
-    end
+# function get_time_in_state(grouped_df)
+#     in_sequence = false
+#     stops = Int[]
+#     starts = Int[]
+#     index_length = length(grouped_df.actual_index)
+#     # println("looping through indexes")
+#     for i in 1:index_length - 1
+#         current_index = grouped_df.actual_index[i]
+#         next_index = grouped_df.actual_index[i + 1]
+#         # println("checking if increasing by 1")
+#         if current_index + 1 == next_index
+#             if !in_sequence
+#                 # Sequence starts increasing by 1
+#                 in_sequence = true
+#                 # println("storing the start index")
+#                 push!(starts, i)  # Store the start index
+#             end
+#         else
+#             if in_sequence
+#                 # println("sequence stops increasing by 1")
+#                 # Sequence stops increasing by 1
+#                 in_sequence = false
+#                 # println("storing the stop index")
+#                 push!(stops, i)  # Store the stop index
+#             end
+#         end
+#     end
 
-    # Handle the case where the sequence ends with the last element
-    # println("sequence ends with last element case")
-    if in_sequence
-        push!(stops, index_length)
-    end
+#     # Handle the case where the sequence ends with the last element
+#     # println("sequence ends with last element case")
+#     if in_sequence
+#         push!(stops, index_length)
+#     end
 
-    # println("calculating total time in each state!")
-    tot_time_in_state = 0
-    for i in eachindex(stops)
-        tot_time_in_state += grouped_df.t[stops[i]]-grouped_df.t[starts[i]]
-    end
+#     # println("calculating total time in each state!")
+#     tot_time_in_state = 0
+#     for i in eachindex(stops)
+#         tot_time_in_state += grouped_df.t[stops[i]]-grouped_df.t[starts[i]]
+#     end
 
-    # println("finished!")
+#     # println("finished!")
+#     return tot_time_in_state
+# end
+function vec_time_in_state(df)
+    d = diff(df.actual_index)
+    in_seq = d .== 1
+    starts_v = findall(diff([false; in_seq]) .== 1)
+    stops_v = findall(diff([in_seq; false]) .== -1) .+ 1
+    tot_time_in_state = sum(df.t[stops_v] .- df.t[starts_v])
     return tot_time_in_state
 end
 function get_grouped_df(data, specie)
@@ -80,11 +88,11 @@ end
 grouped_df, hist_df, bin_edges = get_grouped_df(df, :rh)
 hist_freq = make_hist_freq(bin_edges)
 total_time = hist_df.t[end]
-for (df,i) in ProgressBar(zip(grouped_df, 1:length(grouped_df)))
-    tot_time_in_state = get_time_in_state(df)
+@elapsed for (df,i) in ProgressBar(zip(grouped_df, 1:length(grouped_df)))
+    tot_time_in_state = vec_time_in_state(df)
     hist_freq[i, :freq] = (length(df.t)*tot_time_in_state)/total_time
 end
-# CSV.write("/Users/s2257179/phd/stochastic_hybrid_code/hists/thresh10/rh.csv", hist_freq)
+CSV.write("/home/hollie_hindley/phd/stochastic_hybrid_code/threshold_analysis/histograms/rh_NEW.csv", hist_freq)
 
 using Plots
 histogram(df.rh[1:end], xlims=(800,3e3), bins=50)
@@ -126,11 +134,13 @@ function testing(df)
             end
         else
             if in_sequence
-
                 in_sequence = false
                 push!(stops, i) 
             end
         end
+    end
+    if in_sequence
+        push!(stops, index_length)
     end
     return stops, starts
 end
@@ -139,22 +149,22 @@ end
 # Handle the case where the sequence ends with the last element
 # println("sequence ends with last element case")
 
-function testing_vectorized(df)
-    # Step 1: Calculate differences between consecutive indices
-    diffs = diff(df.actual_index)
+# function testing_vectorized(df)
+#     # Step 1: Calculate differences between consecutive indices
+#     diffs = diff(df.actual_index)
     
-    # Step 2: Identify where the difference is exactly 1 (true for consecutive indices)
-    in_sequence = diffs .== 1
+#     # Step 2: Identify where the difference is exactly 1 (true for consecutive indices)
+#     in_sequence = diffs .== 1
     
-    # Step 3: Find starts - where in_sequence switches from false to true
-    starts = findall(diff([false; in_sequence]) .== 1)
+#     # Step 3: Find starts - where in_sequence switches from false to true
+#     starts = findall(diff([false; in_sequence]) .== 1)
     
-    # Step 4: Find stops - where in_sequence switches from true to false
-    stops = findall(diff([in_sequence; false]) .== -1)
+#     # Step 4: Find stops - where in_sequence switches from true to false
+#     stops = findall(diff([in_sequence; false]) .== -1)
     
-    return stops, starts
-end
-@elapsed stops_v, starts_v = testing_vectorized(df)
+#     return stops, starts
+# end
+# @elapsed stops_v, starts_v = testing_vectorized(df)
 
 diffs = diff(df.actual_index)
 in_sequence = diffs .== 1
@@ -163,14 +173,61 @@ stops = findall(diff([in_sequence; false]) .== -1)
 
 hist_freq
 
+df
 
-ex = DataFrame(x=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.1,1.2,1.3,1.4],y=[1,2,3,4,5,6,7,8,9,15,16,17,18,19])
-d = diff(ex.y)
-in_seq = d .== 1
-sta = findall(diff([false; in_seq]) .== 1)
-sto = findall(diff([in_seq; false]) .== -1)
+ex = DataFrame(actual_index=[1,2,3,4,8,9,15,16,17,18,20,21,22])
 
 
+
+
+
+
+df = grouped_df[1]
+
+function vec_time_in_state(df)
+    d = diff(df.actual_index)
+    in_seq = d .== 1
+    starts_v = findall(diff([false; in_seq]) .== 1)
+    stops_v = findall(diff([in_seq; false]) .== -1) .+ 1
+    tot_time_in_state = sum(df.t[stops_v] .- df.t[starts_v])
+    return tot_time_in_state
+end
+
+tins1 = vec_testing(grouped_df[1])
+
+tins
+
+
+
+
+grouped_df, hist_df, bin_edges = get_grouped_df(df, :rh)
+hist_freq = make_hist_freq(bin_edges)
+total_time = hist_df.t[end]
+for (df,i) in ProgressBar(zip(grouped_df, 1:length(grouped_df)))
+    tot_time_in_state = vec_testing(df)
+    hist_freq[i, :freq] = (length(df.t)*tot_time_in_state)/total_time
+end
+
+hist_freq1
+
+hist_freq1 = DataFrame(CSV.File("/home/hollie_hindley/phd/stochastic_hybrid_code/threshold_analysis/histograms/test_rh.csv"))
+
+
+isequal(hist_freq.freq, hist_freq1.freq)
+
+
+
+
+
+
+
+
+stops1, starts1 = testing(df)
+
+isequal(stops1, stops_v), isequal(starts1, starts_v)
+
+stops1[end-5:end]
+stops_v[end-5:end]
 
 stops
 stops_v
