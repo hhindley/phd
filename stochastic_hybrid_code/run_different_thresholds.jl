584cd5adb9276c6ec8132aeadf334fe1146bbcd2
hhindley <s2257179@ed.ac.uk>
2024-07-18 09:29:12 +0100

    running final tests

--- stochastic_hybrid_code/run_different_thresholds.jl
+++ stochastic_hybrid_code/run_different_thresholds.jl
@@ -7,43 +7,46 @@ include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/hybrid_algo.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/stoch_model.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/file_funcs.jl"))
 
-# n= 10000 # number of cell cycles
-# options = Dict(
-# "threshold"  =>  0.,       # Threshold to decide between determinisitic or stochastic reaction
-# "FixDetReact"=> [14],# [10,11,12,13,14,15,16,17,18],       # Reactions to be treated determinisitically
-#     "tspan"     =>   n*log(2)/lam_val,     # Max time for cell cycle
-#     "samplingFreq"  => 10/60  # for sampling every x mins
-# )
+n= 10000 # number of cell cycles
+options = Dict(
+"threshold"  =>  0.,       # Threshold to decide between determinisitic or stochastic reaction
+"FixDetReact"=> [14],# [10,11,12,13,14,15,16,17,18],       # Reactions to be treated determinisitically
+    "tspan"     =>   n*log(2)/lam_val,     # Max time for cell cycle
+    "samplingFreq"  => 10/60  # for sampling every x mins
+)
 
-# X0 = collect(get_X0(indV, init_molec)')
-# par = collect(get_par(indP)')
+X0 = collect(get_X0(indV, init_molec)')
+par = collect(get_par(indP)')
 
-# println("starting X0 calc")
-# getssX0 = true
-# if getssX0
-#     fout=open(joinpath(homedir(), "/home/hollie_hindley/Documents/stochastic_hybrid/X0.dat"),"w")
-#     propen, S, propList = defineStochModel(par, indV)
-#     nx = indV.nrOfItems-1
-#     prop(X) = propen(X[1:nx])
-#     X0 = hybrid_algo(X0, options, prop, S, out=fout)
-#     X0[vidx(:V)] = 1
-#     CSV.write(joinpath(homedir(), "/home/hollie_hindley/Documents/stochastic_hybrid/X0.dat"), DataFrame(X0,:auto), header=false)
-# else
-#     X0 = CSV.read(joinpath(homedir(), "/home/hollie_hindley/Documents/stochastic_hybrid/X0.dat"), Tables.matrix, header=false)
-# end
+println("starting X0 calc")
+getssX0 = true
+if getssX0
+    fout=open(joinpath(homedir(), "/home/hollie_hindley/Documents/stochastic_hybrid/X0.dat"),"w")
+    propen, S, propList = defineStochModel(par, indV)
+    nx = indV.nrOfItems-1
+    prop(X) = propen(X[1:nx])
+    X0 = hybrid_algo(X0, options, prop, S, out=fout)
+    X0[vidx(:V)] = 1
+    CSV.write(joinpath(homedir(), "/home/hollie_hindley/Documents/stochastic_hybrid/X0.dat"), DataFrame(X0,:auto), header=false)
+else
+    X0 = CSV.read(joinpath(homedir(), "/home/hollie_hindley/Documents/stochastic_hybrid/X0.dat"), Tables.matrix, header=false)
+end
 
-# println("finished X0 calc")
+println("finished X0 calc")
 
 
 # threshold_vals = range(10,310,length=20)
 # threshold_vals_new = collect(range(threshold_vals[10], threshold_vals[14], length=5))
 # pushfirst!(threshold_vals_new, 160)
 # push!(threshold_vals_new, 210)
-threshold_vals = range(100,250,length=10)
+
+# threshold_vals = range(100,250,length=10) # 16/07 and 18/07 threshold values
+threshold_vals = range(125, 170, length=10) # 18/07 smaller range threshold values 
 
 
-mainpath = "/home/hollie_hindley/Documents/stochastic_hybrid/"
-dir = "1607_tests" # change this! 
+mainpath = "/home/hollie_hindley/Documents/stochastic_hybrid/threshold_testing"
+dir = "1807_smaller_range" # change this! 
+print(dir)
 folderpath = joinpath(mainpath, dir)
 if !isdir(folderpath)
     mkdir(folderpath)
@@ -53,17 +56,17 @@ final_path = dir * "_final_files"
 
 
 df = DataFrame(threshold=threshold_vals, time=zeros(length(threshold_vals)))
-# # Threads.@threads :static for i in eachindex(threshold_vals_new)
-# for i in eachindex(threshold_vals)
-#     println("starting $i")
-#     time_taken = @elapsed run_stoch(X0, threshold_vals[i], 0.05, joinpath(folderpath,"thresh_$(threshold_vals[i]).dat"))
-#     df.time[i] = time_taken
-#     println("finished $i")
-# end
+# Threads.@threads :static for i in eachindex(threshold_vals_new)
+for i in eachindex(threshold_vals)
+    println("starting $i")
+    time_taken = @elapsed run_stoch(X0, threshold_vals[i], 0.05, joinpath(folderpath,"thresh_$(threshold_vals[i]).dat"))
+    df.time[i] = time_taken
+    println("finished $i")
+end
 
 CSV.write(joinpath(mainpath, "$time_file"), df)
 
-# println("total time = $(sum(df.time)/60/60) hours")
+println("total time = $(sum(df.time)/60/60) hours")
 
 println("starting file conversion")
 
