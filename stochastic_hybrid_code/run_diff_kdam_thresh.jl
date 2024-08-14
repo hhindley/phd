27a686a6b7da1420a4a4937ada6472614b70e453
hhindley <s2257179@ed.ac.uk>
2024-08-14 11:57:45 +0100

    bug fix

--- stochastic_hybrid_code/run_diff_kdam_thresh.jl
+++ stochastic_hybrid_code/run_diff_kdam_thresh.jl
@@ -8,6 +8,7 @@ include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/stoch_model.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/file_funcs.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/threshold_analysis/histograms/make_hists.jl"))  
 
+n= 10000 # number of cell cycles
 options = Dict(
 "threshold"  =>  0.,       # Threshold to decide between determinisitic or stochastic reaction
 "FixDetReact"=> [14],# [10,11,12,13,14,15,16,17,18],       # Reactions to be treated determinisitically
