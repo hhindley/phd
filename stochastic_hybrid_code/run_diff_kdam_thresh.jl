617016a87722aed17c350747f7c79e9e77def79d
hhindley <s2257179@ed.ac.uk>
2024-08-14 11:55:52 +0100

    added time of day to simulations

--- stochastic_hybrid_code/run_diff_kdam_thresh.jl
+++ stochastic_hybrid_code/run_diff_kdam_thresh.jl
@@ -1,4 +1,4 @@
-using StatsBase, Distributions, Random, DataFrames, CSV, PlotlyJS, DifferentialEquations, OrderedCollections, ProgressBars, BenchmarkTools
+using Dates, StatsBase, Distributions, Random, DataFrames, CSV, PlotlyJS, DifferentialEquations, OrderedCollections, ProgressBars, BenchmarkTools
 
 include(joinpath(homedir(), "phd/rtc_model/parameters/rtc_params.jl"))
 include(joinpath(homedir(), "phd/rtc_model/parameters/rtc_params_molecs.jl"))
@@ -6,10 +6,8 @@ include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/indexing.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/hybrid_algo.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/stoch_model.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/file_funcs.jl"))
-include(joinpath(homedir(), "phd/stochastic_hybrid_code/threshold_analysis/histograms/make_hists.jl"))
+include(joinpath(homedir(), "phd/stochastic_hybrid_code/threshold_analysis/histograms/make_hists.jl"))  
 
-
-n= 10000 # number of cell cycles
 options = Dict(
 "threshold"  =>  0.,       # Threshold to decide between determinisitic or stochastic reaction
 "FixDetReact"=> [14],# [10,11,12,13,14,15,16,17,18],       # Reactions to be treated determinisitically
@@ -38,7 +36,7 @@ println("finished X0 calc")
 
 
 mainpath = "/home/hollie_hindley/Documents/stochastic_hybrid/kdam_testing"
-dir = "kdam_and_thresh_test_1308" # change this! 
+dir = "kdam_and_thresh_test_1408" # change this! 
 folderpath = joinpath(mainpath, dir)
 if !isdir(folderpath)
     mkdir(folderpath)
@@ -47,20 +45,20 @@ time_file = dir * "_times.csv"
 final_path = dir * "_final_files"
 
 
-kdam_vals = [0.005, 0.01, 0.03, 0.05, 0.075, 0.1, 0.3, 0.5]
+kdam_vals = [0.075, 0.1, 0.1, 0.3, 0.5]
 
 # thresh_scaling = [14000, 13500, 13000, 12500, 12000, 11500, 11000, 3800, 2000, 440]
 
 # thresh_vals = kdam_vals.*thresh_scaling
 
-thresh_vals = [50, 100, 125, 150, 160, 170, 180, 190]
+thresh_vals = [150, 150, 160, 160, 170]
 
 df = DataFrame(kdam=kdam_vals, thresh=thresh_vals, time=zeros(length(kdam_vals)))
 for i in eachindex(kdam_vals)
-    println("starting $i")
+    println("starting $i, $(Dates.now())")
     time_taken = @elapsed run_stoch(X0, thresh_vals[i], kdam_vals[i], joinpath(folderpath,"kdam_$(kdam_vals[i])_thresh_$(thresh_vals[i]).dat"))
     df.time[i] = time_taken
-    println("finished $i")
+    println("finished $i, $(Dates.now())")
 end
 
 CSV.write(joinpath(mainpath, "$time_file"), df)
