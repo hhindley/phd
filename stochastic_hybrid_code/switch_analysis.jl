593899c1210db4f3490d0ddf696d50dd7cbd0f72
hhindley <s2257179@ed.ac.uk>
2024-08-29 17:00:54 +0100

    more switch analysis

--- stochastic_hybrid_code/switch_analysis.jl
+++ stochastic_hybrid_code/switch_analysis.jl
@@ -12,7 +12,7 @@ all_items = readdir(mount_path)
 folders = [item for item in all_items if isdir(joinpath(mount_path, item)) && !occursin("DS", item)]
 folders_dict = Dict(i => folder for (i, folder) in enumerate(folders))
 
-folders_dict = Dict(filter(pair -> pair.first in [6], folders_dict))
+folders_dict = Dict(filter(pair -> pair.first in [6,7,8,9], folders_dict))
 
 dict_times, dict_kdamvals, dict_titles, dict_results, dict_reacts, dict_props, dict_counts, dict_hists = setup_dicts(folders_dict)
 
@@ -25,8 +25,8 @@ end
 
 # plot one result
 folder = 6; index = 1; species = "rtca"; num_plots = 1;
-f = plot_results("plot_results", dict_results[folder][index], num_plots, folders_dict[folder], titles=[dict_titles[folder][index]], species="$species", xlabel="time", ylabel="$species", size=(800,650), tosave=false)
-f_rib = plot_results("plot_results", dict_results[folder][index], 1, folders_dict[folder], titles=[dict_titles[folder][index]], species=[:rh, :rd, :rt], xlabel="time", ylabel="specie", size=(800,650), tosave=false, linkaxes=false, conc=true)
+f = plot_results("plot_results", dict_results[folder][index], num_plots, folders_dict[folder], titles=[dict_titles[folder][index]], species="$species", xlabel="time", ylabel="$species", size=(800,650), tosave=false, conc=true)
+f_rib = plot_results("plot_results", dict_results[folder][index], 1, folders_dict[folder], titles=[dict_titles[folder][index]], species=[:rh, :rd, :rt], xlabel="time", ylabel="specie", size=(800,650), tosave=false, linkaxes=false, conc=false)
 f_mrna = plot_results("plot_results", dict_results[folder][index], 1, folders_dict[folder], species=[:rm_a, :rm_b, :rm_r], xlabel="time", ylabel="specie", size=(800,650), tosave=false, linkaxes=false, conc=true)
 f_prot = plot_results("plot_results", dict_results[folder][index], 1, folders_dict[folder], titles=[dict_titles[folder][index]], species=[:rtca, :rtcb, :rtcr], xlabel="time", ylabel="specie", size=(800,650), tosave=false, linkaxes=false, conc=false)
 
@@ -75,8 +75,7 @@ function switch_times(res; threshold=nothing)
     condition_changes = diff(Int.(condition))
     start_indices = findall(x -> x == 1, condition_changes)
     stop_indices = findall(x -> x == -1, condition_changes)
-    start_times = [df.time[1]]
-    # start_times = []
+    start_times = []
     for i in start_indices
         push!(start_times, df.time[i])
     end
@@ -84,15 +83,26 @@ function switch_times(res; threshold=nothing)
     for i in stop_indices
         push!(stop_times, df.time[i])
     end
-    nan_row = DataFrame(on=NaN, time=NaN, rtca=NaN, rtcb=NaN)
-    df = vcat(nan_row, df)
+    if start_times[1] > stop_times[1] && start_times[end] > stop_times[end]
+        println("start_times[1] > stop_times[1] && start_times[end] > stop_times[end]")
+        pushfirst!(start_times, df.time[1])
+        push!(stop_times, df.time[end])
+    elseif start_times[end] > stop_times[end]
+        println("start_times[end] > stop_times[end]")
+        push!(stop_times, df.time[end])
+    elseif start_times[1] > stop_times[1] 
+        println("start_times[1] > stop_times[1] ")
+        pushfirst!(start_times, df.time[1])
+    end
     return df, start_times, stop_times
 end
-threshold=2
-df, start_times, stop_times = switch_times(dict_results[folder][6], threshold=threshold)
+threshold=5
+df, start_times, stop_times = switch_times(dict_results[7][2], threshold=threshold)
 df1, start_times1, stop_times1 = switch_times(dict_results[folder][6])
 
-function calc_switch_rate(start_times, stop_times)
+start_times
+stop_times
+function calc_switch_rate(df, start_times, stop_times)
     time_on = sum(stop_times.-start_times)
     on_res = time_on/length(start_times)
     time_off = df.time[end] - time_on
@@ -102,13 +112,13 @@ function calc_switch_rate(start_times, stop_times)
     return switch_rate_on, switch_rate_off
 end
 
-switch_rate_on, switch_rate_off = calc_switch_rate(start_times, stop_times)
-switch_rate_on1, switch_rate_off1 = calc_switch_rate(start_times1, stop_times1)
+switch_rate_on, switch_rate_off = calc_switch_rate(df, start_times, stop_times)
+switch_rate_on1, switch_rate_off1 = calc_switch_rate(df1, start_times1, stop_times1)
 
 
 # plot switch times
 f = Figure()
-ax = Axis(f[1,1], title="Threshold for on state = $threshold, time on = $(round(time_on, digits=2)), switch rate = $(round(switch_rate_on, digits=6))", xlabel="time", ylabel="molecule number")
+ax = Axis(f[1,1], title="Threshold for on state = $threshold, switch rate = $(round(switch_rate_on, digits=6))", xlabel="time", ylabel="molecule number")
 lines!(ax, df.time, df.rtca, label="rtca")
 lines!(ax, df.time, df.rtcb, label="rtcb")
 scatter!(ax, start_times, ones(length(start_times)), markersize=10, label="start on")
@@ -169,14 +179,18 @@ function get_all_switch_rates(folder; threshold=nothing)
     for i in eachindex(dict_results[folder])
         println(i)
         df, start_times, stop_times = switch_times(dict_results[folder][i], threshold=threshold)
-        switch_rate_on, switch_rate_off = calc_switch_rate(start_times, stop_times)
+        switch_rate_on, switch_rate_off = calc_switch_rate(df, start_times, stop_times)
         push!(switch_rates_on, switch_rate_on)
         push!(switch_rates_off, switch_rate_off)
     end
     return switch_rates_on, switch_rates_off
 end
 
-switch_vals_on, switch_vals_off = get_all_switch_rates(6, threshold=threshold)
+switch_vals_on6, switch_vals_off6 = get_all_switch_rates(6, threshold=threshold)
+switch_vals_on7, switch_vals_off7 = get_all_switch_rates(7, threshold=threshold)
+switch_vals_on8, switch_vals_off8 = get_all_switch_rates(8, threshold=threshold)
+switch_vals_on9, switch_vals_off9 = get_all_switch_rates(9, threshold=threshold)
+
 switch_vals_on1, switch_vals_off1 = get_all_switch_rates(6)
 
 f = Figure()
@@ -191,12 +205,18 @@ lines!(ax, dict_kdamvals[folder][:kdam], switch_vals_off, label="threshold metho
 lines!(ax, dict_kdamvals[folder][:kdam], switch_vals_off1, label="difference method")
 axislegend()
 
-f = Figure()
-ax = Axis(f[1,1], xlabel="kdam", ylabel="switch rate", title="threshold method", yscale=log10)
-lines!(ax, dict_kdamvals[folder][:kdam], switch_vals_on, label="on→off")
-lines!(ax, dict_kdamvals[folder][:kdam], switch_vals_off, label="off→on")
-axislegend()
-
+f_switch = Figure()
+ax = Axis(f_switch[1,1], xlabel="kdam", ylabel="switch rate", title="threshold method", yscale=log10)
+lines!(ax, dict_kdamvals[6][:kdam], switch_vals_on6, label="on→off 6")
+lines!(ax, dict_kdamvals[6][:kdam], switch_vals_off6, label="off→on 6")
+lines!(ax, dict_kdamvals[7][:kdam], switch_vals_on7, label="on→off 7")
+lines!(ax, dict_kdamvals[7][:kdam], switch_vals_off7, label="off→on 7")
+lines!(ax, dict_kdamvals[8][:kdam], switch_vals_on8, label="on→off 8")
+lines!(ax, dict_kdamvals[8][:kdam], switch_vals_off8, label="off→on 8")
+lines!(ax, dict_kdamvals[9][:kdam], switch_vals_on9, label="on→off 9")
+lines!(ax, dict_kdamvals[9][:kdam], switch_vals_off9, label="off→on 9")
+axislegend(position=:rc)
+display(GLMakie.Screen(), f_switch)
 
 f = Figure()
 ax = Axis(f[1,1], xlabel="kdam", ylabel="switch rate", title="difference method", yscale=log10)
@@ -204,14 +224,128 @@ lines!(ax, dict_kdamvals[folder][:kdam], switch_vals_on1, label="on→off")
 lines!(ax, dict_kdamvals[folder][:kdam], switch_vals_off1, label="off→on")
 axislegend()
 
+function calc_frac_times(switch_vals_on, switch_vals_off)
+    frac_on = @. switch_vals_off/(switch_vals_on+switch_vals_off)
+    frac_off = @. switch_vals_on/(switch_vals_on+switch_vals_off)
+    return frac_on, frac_off
+end
 
-frac_on = @. switch_vals_off/(switch_vals_on+switch_vals_off)
-frac_off = @. switch_vals_on/(switch_vals_on+switch_vals_off)
+frac_on6, frac_off6 = calc_frac_times(switch_vals_on6, switch_vals_off6)
+frac_on7, frac_off7 = calc_frac_times(switch_vals_on7, switch_vals_off7)
+frac_on8, frac_off8 = calc_frac_times(switch_vals_on8, switch_vals_off8)
+frac_on9, frac_off9 = calc_frac_times(switch_vals_on9, switch_vals_off9)
+
+f_frac = Figure()
+ax = Axis(f_frac[1,1], xlabel="kdam", ylabel="fraction of time in state", title="threshold method")
+lines!(ax, dict_kdamvals[6][:kdam], frac_on6, label="on state6")
+lines!(ax, dict_kdamvals[6][:kdam], frac_off6, label="off state6")
+lines!(ax, dict_kdamvals[7][:kdam], frac_on7, label="on state7")
+lines!(ax, dict_kdamvals[7][:kdam], frac_off7, label="off state7")
+lines!(ax, dict_kdamvals[8][:kdam], frac_on8, label="on state8")
+lines!(ax, dict_kdamvals[8][:kdam], frac_off8, label="off state8")
+lines!(ax, dict_kdamvals[9][:kdam], frac_on9, label="on state9")
+lines!(ax, dict_kdamvals[9][:kdam], frac_off9, label="off state9")
+axislegend(position=:rc)
+display(GLMakie.Screen(), f_frac)
+frac_on6+frac_off6
+dict_reacts[6][1]
+
+dict_results[6][1]
+
+
+function get_av_conc_state(res; on=true)
+    df = res[!,[:time, :rtca, :rtcb, :volume]]
+    if threshold !== nothing
+        condition = (df.rtca .> threshold) .| (df.rtcb .> threshold)
+    else
+        diff_rtca = diff(df.rtca)
+        diff_rtcb = diff(df.rtcb)
+        condition = (diff_rtca .> 1e-10) .| (diff_rtcb .> 1e-10)
+        pushfirst!(condition, !condition[1])
+    end
+    df = insertcols!(df, 1, :on => condition)
+    condition_changes = diff(Int.(condition))
+    start_indices = findall(x -> x == 1, condition_changes)
+    stop_indices = findall(x -> x == -1, condition_changes)
+
+    if start_indices[1] > stop_indices[1] && start_indices[end] > stop_indices[end]
+        # println("start_times[1] > stop_times[1] && start_times[end] > stop_times[end]")
+        pushfirst!(start_indices, 1)
+        push!(stop_indices, length(df.time))
+    elseif start_indices[end] > stop_indices[end]
+        # println("start_times[end] > stop_times[end]")
+        push!(stop_indices, length(df.time))
+    elseif start_indices[1] > stop_indices[1] 
+        # println("start_times[1] > stop_times[1] ")
+        pushfirst!(start_indices, 1)
+    end
+
+    if on
+        rtca_vals = [df.rtca[start:stop] for (start, stop) in zip(start_indices, stop_indices)]
+        rtcb_vals = [df.rtcb[start:stop] for (start, stop) in zip(start_indices, stop_indices)]
+        vol_vals = [df.volume[start:stop] for (start, stop) in zip(start_indices, stop_indices)]
+
+        rtca_mean = mean([mean(rtca ./ volume) for (rtca, volume) in zip(rtca_vals, vol_vals)])
+        rtcb_mean = mean([mean(rtcb ./ volume) for (rtcb, volume) in zip(rtcb_vals, vol_vals)])
+    else
+        new_starts = start_indices[2:end]
+        new_stops = stop_indices[1:end-1]
+        rtca_vals = [df.rtca[stop:start] for (start, stop) in zip(new_starts, new_stops)]
+        rtcb_vals = [df.rtcb[stop:start] for (start, stop) in zip(new_starts, new_stops)]
+        vol_vals = [df.volume[stop:start] for (start, stop) in zip(new_starts, new_stops)]
+
+        rtca_mean = mean([mean(rtca ./ volume) for (rtca, volume) in zip(rtca_vals, vol_vals)])
+        rtcb_mean = mean([mean(rtcb ./ volume) for (rtcb, volume) in zip(rtcb_vals, vol_vals)])
+    end
+
+    return rtca_mean, rtcb_mean
+end
+
+rtca_on, rtcb_on = get_av_conc_state(dict_results[6][1], on=true)
+rtca_off, rtcb_off = get_av_conc_state(dict_results[6][1], on=false)
+
+
+rtc_on6 = [get_av_conc_state(dict_results[6][i], on=true) for i in eachindex(dict_results[6])]
+rtc_off6 = [get_av_conc_state(dict_results[6][i], on=false) for i in eachindex(dict_results[6])]
+
+rtc_on7 = [get_av_conc_state(dict_results[7][i], on=true) for i in eachindex(dict_results[7])]
+rtc_off7 = [get_av_conc_state(dict_results[7][i], on=false) for i in eachindex(dict_results[7])]
+
+rtc_on8 = [get_av_conc_state(dict_results[8][i], on=true) for i in eachindex(dict_results[8])]
+rtc_off8 = [get_av_conc_state(dict_results[8][i], on=false) for i in eachindex(dict_results[8])]
+
+rtc_on9 = [get_av_conc_state(dict_results[9][i], on=true) for i in eachindex(dict_results[9])]
+rtc_off9 = [get_av_conc_state(dict_results[9][i], on=false) for i in eachindex(dict_results[9])]
+
+
+rtca_on6 = [rtc_on6[i][1] for i in eachindex(rtc_on6)]
+rtca_on7 = [rtc_on7[i][1] for i in eachindex(rtc_on7)]
+rtca_on8 = [rtc_on8[i][1] for i in eachindex(rtc_on8)]
+rtca_on9 = [rtc_on9[i][1] for i in eachindex(rtc_on9)]
+
+rtcb_on6 = [rtc_on6[i][2] for i in eachindex(rtc_on6)]
+rtcb_on7 = [rtc_on7[i][2] for i in eachindex(rtc_on7)]
+rtcb_on8 = [rtc_on8[i][2] for i in eachindex(rtc_on8)]
+rtcb_on9 = [rtc_on9[i][2] for i in eachindex(rtc_on9)]
+
+rtca_off6 = [rtc_off6[i][1] for i in eachindex(rtc_off6)]
+rtca_off7 = [rtc_off7[i][1] for i in eachindex(rtc_off7)]
+rtca_off8 = [rtc_off8[i][1] for i in eachindex(rtc_off8)]
+rtca_off9 = [rtc_off9[i][1] for i in eachindex(rtc_off9)]
+
+rtcb_off6 = [rtc_off6[i][2] for i in eachindex(rtc_off6)]
+rtcb_off7 = [rtc_off7[i][2] for i in eachindex(rtc_off7)]
+rtcb_off8 = [rtc_off8[i][2] for i in eachindex(rtc_off8)]
+rtcb_off9 = [rtc_off9[i][2] for i in eachindex(rtc_off9)]
 
 f = Figure()
-ax = Axis(f[1,1], xlabel="kdam", ylabel="fraction of time in state", title="threshold method")
-lines!(ax, dict_kdamvals[folder][:kdam], frac_on, label="on state")
-lines!(ax, dict_kdamvals[folder][:kdam], frac_off, label="off state")
-axislegend()
+ax = Axis(f[1,1], yscale=log10)
+scatter!(ax, dict_kdamvals[6][:kdam], rtca_on6, color=frac_on6)
+scatter!(ax, dict_kdamvals[6][:kdam], rtca_on7, color=frac_on7)
+scatter!(ax, dict_kdamvals[6][:kdam], rtca_on8, color=frac_on8)
+scatter!(ax, dict_kdamvals[6][:kdam], rtca_on9, color=frac_on9)
 
-
+scatter!(ax, dict_kdamvals[6][:kdam], rtca_off6, color=frac_off6)
+scatter!(ax, dict_kdamvals[6][:kdam], rtca_off7, color=frac_off7)
+scatter!(ax, dict_kdamvals[6][:kdam], rtca_off8, color=frac_off8)
+scatter!(ax, dict_kdamvals[6][:kdam], rtca_off9, color=frac_off9)
--- stochastic_hybrid_code/threshold_analysis/stoch_reacts/plot_reacts.jl
+++ stochastic_hybrid_code/threshold_analysis/stoch_reacts/plot_reacts.jl
@@ -107,3 +107,30 @@ p1 = plot_results("plot_individual_reacts", dfs0, 5, 3, titles=titles_reacts, xl
 p2 = plot_results("plot_individual_reacts", dfs, 5, 3, titles=titles_reacts, xlabel="threshold", ylabel="count", hidelabels=[true, false], linkaxes=false, folder="thresh_plots2", species="individual_reacts")
 
 display(GLMakie.Screen(), p1.plot)
+
+
+
+# fraction of stoch reacts for each condition
+function calc_frac_stoch_react(folder)
+    for df in eachindex(dict_reacts[folder])
+        fraction = [i/sum(dict_reacts[folder][df].count) for i in dict_reacts[folder][1].count]
+        dict_reacts[folder][df] = insertcols!(dict_reacts[folder][df], 4, :fraction => fraction)
+    end
+end
+
+calc_frac_stoch_react(6)
+calc_frac_stoch_react(7)
+calc_frac_stoch_react(8)
+calc_frac_stoch_react(9)
+
+colors = Makie.wong_colors()
+labels = ["kdam = $i" for i in dict_kdamvals[6][:kdam]]
+events = [event for df in dict_reacts[6] for event in df.event]
+fractions = [fraction for df in dict_reacts[6] for fraction in df.fraction]
+groups = repeat(Int.(collect(range(1, length(dict_reacts[6]), length=length(dict_reacts[6])))), inner=length(dict_reacts[6][1].event))
+elements = [PolyElement(polycolor = colors[i]) for i in 1:length(labels)]
+
+f = Figure()
+ax = Axis(f[1,1], xticks=(1:13, react_names_str), xticklabelrotation=45)
+barplot!(ax, events, fractions, dodge=groups, color=colors[groups])
+Legend(f[1,2], elements, labels)
