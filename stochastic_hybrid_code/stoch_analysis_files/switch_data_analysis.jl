eba797953ef5bb5b2e56a99dd6c89501fad41dfa
hhindley <s2257179@ed.ac.uk>
2024-09-17 14:34:40 +0100

    switch data plots

--- stochastic_hybrid_code/stoch_analysis_files/switch_data_analysis.jl
+++ stochastic_hybrid_code/stoch_analysis_files/switch_data_analysis.jl
@@ -5,6 +5,7 @@ using JLD2, InteractiveViz, GLMakie, Statistics
 @load "/Users/s2257179/Desktop/saved_variables/data_thresh_10.jld2" all_start_indices10 all_stop_indices10 all_switch_rates_on10 all_switch_rates_off10 all_fracs_on10 all_fracs_off10 all_species_mean_on10 all_species_mean_off10
 @load "/Users/s2257179/Desktop/saved_variables/data_thresh_bs.jld2" all_start_indices_bs all_stop_indices_bs all_switch_rates_on_bs all_switch_rates_off_bs all_fracs_on_bs all_fracs_off_bs all_species_mean_on_bs all_species_mean_off_bs thresholds_rtca thresholds_rtcb
 
+delete!(all_switch_rates_on_bs, 10) # kdam 1.1 in folder 10 for bs had a bigger value for some reason so remove this
 # sorting results for plotting
 kdams = [0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]
 
@@ -99,12 +100,6 @@ f_frac10 = plot_mean_std(mean_switch_frac_on10, std_switch_frac_on10, mean_switc
 f_switch_bs = plot_mean_std(mean_switch_frac_on_bs, std_switch_frac_on_bs, mean_switch_frac_off_bs, std_switch_frac_off_bs, "switch")
 f_frac_bs = plot_mean_std(mean_switch_frac_on_bs, std_switch_frac_on_bs, mean_switch_frac_off_bs, std_switch_frac_off_bs, "frac")
 
-errorbars(kdams, [mean_switch_frac_on_bs["switch"][key] for key in sorted_keys], [std_switch_frac_on_bs["switch"][key] for key in sorted_keys])
-lines!(kdams, [mean_switch_frac_on_bs["switch"][key] for key in sorted_keys])
-std_switch_frac_on_bs["switch"][1.1]
-
-minimum([mean_switch_frac_on_bs["switch"][key] for key in sorted_keys])
-minimum([std_switch_frac_on_bs["switch"][key] for key in sorted_keys])
 
 # plotting all results
 f_switch = Figure()
@@ -116,8 +111,8 @@ display(GLMakie.Screen(), f_switch)
 
 f_frac = Figure()
 ax = Axis(f_frac[1,1], xlabel="kdam", ylabel="fraction of time in state", title="threshold method")
-[lines!(ax, kdams, [all_fracs_on[i][key] for key in kdams], label="on state $i") for i in eachindex(all_fracs_on)]
-[lines!(ax, kdams, [all_fracs_off[i][key] for key in kdams], label="off state $i") for i in eachindex(all_fracs_off)]
+[lines!(ax, kdams, [all_fracs_on2[i][key] for key in kdams], label="on state $i") for i in eachindex(all_fracs_on2)]
+[lines!(ax, kdams, [all_fracs_off2[i][key] for key in kdams], label="off state $i") for i in eachindex(all_fracs_off2)]
 axislegend(position=:rc)
 display(GLMakie.Screen(), f_frac)
 
@@ -132,6 +127,8 @@ function plot_conc_frac(specie_plot, all_species_mean_on, all_species_mean_off, 
 
     return f
 end
+
+delete!(all_species_mean_on2, 10)
 
 f2_rtca = plot_conc_frac(1, all_species_mean_on2, all_species_mean_off2, all_fracs_on2, all_fracs_off2)
 f5_rtca = plot_conc_frac(1, all_species_mean_on5, all_species_mean_off5, all_fracs_on5, all_fracs_off5)
