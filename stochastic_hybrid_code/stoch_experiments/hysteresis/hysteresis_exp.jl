a57781a7c6ad5bd273519cf72d68606498dbbf3e
hhindley <s2257179@ed.ac.uk>
2024-09-17 16:39:41 +0100

    hysteresis exp

--- stochastic_hybrid_code/stoch_experiments/hysteresis/hysteresis_exp.jl
+++ stochastic_hybrid_code/stoch_experiments/hysteresis/hysteresis_exp.jl
@@ -6,7 +6,7 @@ include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/indexing.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/hybrid_algo.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/stoch_model.jl"))
 include(joinpath(homedir(), "phd/stochastic_hybrid_code/setup/file_funcs.jl"))
-include(joinpath(homedir(), "phd/stochastic_hybrid_code/threshold_analysis/histograms/make_hists.jl"))
+include(joinpath(homedir(), "phd/stochastic_hybrid_code/stoch_analysis_files/histograms/make_hists.jl"))
 
 
 n= 10000 # number of cell cycles
@@ -14,7 +14,7 @@ options = Dict(
 "threshold"  =>  0.,       # Threshold to decide between determinisitic or stochastic reaction
 "FixDetReact"=> [14],# [10,11,12,13,14,15,16,17,18],       # Reactions to be treated determinisitically
     "tspan"     =>   n*log(2)/lam_val,     # Max time for cell cycle
-    "samplingFreq"  => 10/60  # for sampling every x mins
+    "samplingFreq"  => 1 # for sampling every x mins
 )
 
 X0 = collect(get_X0(indV, init_molec)')
@@ -36,15 +36,26 @@ end
 println("finished X0 calc, X0: $X0")
 
 mainpath = "/home/hollie_hindley/Documents/stochastic_hybrid/hysteresis/"
-dir = "2808_high1.5-0.8" # change this! 
+date = Dates.format(Dates.now(), "ddmm")
+
+high = false
+num = 5
+
+if high 
+    dir_num = "high_$num"
+    kdam_range1 = [1.5, 0.8]
+else 
+    dir_num = "low_$num"
+    kdam_range1 = [0.01, 0.8]
+end
+
+dir = "$(date)_$dir_num" 
 folderpath = joinpath(mainpath, dir)
 if !isdir(folderpath)
     mkdir(folderpath)
 end
 time_file = dir * "_times.csv"
 final_path = dir * "_final_files"
-
-kdam_range1 = [1.5, 0.8]
 
 df = DataFrame(kdam=kdam_range1, time=zeros(length(kdam_range1)))
 for i in eachindex(kdam_range1)
@@ -53,9 +64,7 @@ for i in eachindex(kdam_range1)
     time_taken = @elapsed run_stoch(X0, 150, kdam_range1[i], joinpath(folderpath,"kdam_$(kdam_range1[i]).dat"))    
     df.time[i] = time_taken 
     df_ss = CSV.read(joinpath(folderpath,"kdam_$(kdam_range1[i]).dat"), DataFrame; header=false)
-    ss_region_start = Int(round(length(df_ss[!,1])*0.9))
-    ss_region = df_ss[ss_region_start:end,:]
-    init1 = [mean(ss_region[!,col]) for col in names(ss_region[:,3:end-2])]
+    init1 = [df_ss[end,col] for col in names(ss_region[:,3:end-2])]
     global X0 = collect(get_X0(indV, init1)')
     println("calculated new X0 and finished $i, $(Dates.now())")
 end
