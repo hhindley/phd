cbebedf9ab28d0ec63ce724dd87078941d7e272b
hhindley <s2257179@ed.ac.uk>
2024-09-23 15:04:18 +0100

    numerical bistab analysis and updated kdam vary

--- rtc_model/rhlam_coupled/bistab_search/kdam_vary_search.jl
+++ rtc_model/rhlam_coupled/bistab_search/kdam_vary_search.jl
@@ -1,67 +1,73 @@
-using JLD2, GLMakie, InteractiveViz
-include(joinpath(homedir(), "phd/rtc_model/rhlam_coupled/bistab_search/setup.jl"))
+using JLD2
+# using GLMakie, InteractiveViz
+model = "lamkin_coupled"
+include(joinpath(homedir(), "phd/rtc_model/rhlam_coupled/models/$model.jl"))
+lam_c_vals = 10 .^range(log10(1e-8),log10(0.1), length=5) #8e-7
+kin_c_vals = 10 .^range(log10(1e-6),log10(0.1), length=5) #1.5e-5
+wab_vals = 10 .^range(log10(1e-6),log10(0.1), length=5) #1.3e-5
+kdam_range = range(0,100,length=20)
 
+lamkin_ssvals = []
+lamkin_vals = []
 
-# lamkin_ssvals = []
-# lamkin_vals = []
-
-# for i in lam_c_vals
-#     for j in kin_c_vals
-#         for wab in wab_vals
-#             test_params = deepcopy(params_rtc1)
-#             test_params[lam_c] = i
-#             test_params[kin_c] = j
-#             test_params[ω_ab] = wab
-#             push!(lamkin_vals, (i,j,wab))
-#             res = var_param(test, kdam, test_params, kdam_range, ssvals_rtc)
-#             push!(lamkin_ssvals, res.rtca)
-#         end
-#     end
-# end
-
-
-# @save "/home/hollie_hindley/phd/rtc_model/rhlam_coupled/bistab_search/kdam_vary_search.jld2" lamkin_ssvals lamkin_vals
-
-
-@load joinpath(homedir(),"phd/rtc_model/rhlam_coupled/bistab_search/kdam_vary_search.jld2") lamkin_ssvals lamkin_vals
-
-
-plot([scatter(x=kdam_range, y=lamkin_ssvals[i], name="lam_c, kin_c, wab = $(lamkin_vals[i])") for i in eachindex(lamkin_vals)], Layout(xaxis_title="kdam", yaxis_title="rtca"))
-
-
-
-
-function bistab_check(ssvals)
-    m, m_ind = findmax(ssvals)
-    for i in ssvals[m_ind:end]
-        if ((i-m)/m*100) < -50
-            return true
+for i in lam_c_vals
+    for j in kin_c_vals
+        for wab in wab_vals
+            test_params = deepcopy(params_rtc1)
+            test_params[lam_c] = i
+            test_params[kin_c] = j
+            test_params[ω_ab] = wab
+            push!(lamkin_vals, (i,j,wab))
+            ssvals_rtc = steady_states(lamkin_coupled, init_rtc, test_params)
+            res = var_param(lamkin_coupled, kdam, test_params, kdam_range, ssvals_rtc)
+            push!(lamkin_ssvals, res.rtca)
         end
     end
-    return false
 end
-indices=[]
-for i in eachindex(lamkin_ssvals)
-    if bistab_check(lamkin_ssvals[i])
-        push!(indices, i)
-    end
-end
-indices
-
-f = Figure()
-ax = Axis(f[1, 1])
-[ilines!(ax, kdam_range, lamkin_ssvals[i]) for i in indices]
-
-lamkin_vals[indices]
 
 
-f = Figure()
-ax = Axis(f[1, 1])
-lines!(ax, kdam_range, lamkin_ssvals[indices[4]])
+@save "/home/hollie_hindley/phd/rtc_model/rhlam_coupled/bistab_search/kdam_vary_search.jld2" lamkin_ssvals lamkin_vals
 
-lamkin_vals[indices[5]]
 
-lam_c_val = lamkin_vals[indices[5]][1]
-kin_c_val = lamkin_vals[indices[5]][2]
-ω_ab_val = lamkin_vals[indices[5]][3]
+# @load joinpath(homedir(),"phd/rtc_model/rhlam_coupled/bistab_search/kdam_vary_search.jld2") lamkin_ssvals lamkin_vals
+
+
+# plot([scatter(x=kdam_range, y=lamkin_ssvals[i], name="lam_c, kin_c, wab = $(lamkin_vals[i])") for i in eachindex(lamkin_vals)], Layout(xaxis_title="kdam", yaxis_title="rtca"))
+
+
+
+
+# function bistab_check(ssvals)
+#     m, m_ind = findmax(ssvals)
+#     for i in ssvals[m_ind:end]
+#         if ((i-m)/m*100) < -50
+#             return true
+#         end
+#     end
+#     return false
+# end
+# indices=[]
+# for i in eachindex(lamkin_ssvals)
+#     if bistab_check(lamkin_ssvals[i])
+#         push!(indices, i)
+#     end
+# end
+# indices
+
+# f = Figure()
+# ax = Axis(f[1, 1])
+# [ilines!(ax, kdam_range, lamkin_ssvals[i]) for i in indices]
+
+# lamkin_vals[indices]
+
+
+# f = Figure()
+# ax = Axis(f[1, 1])
+# lines!(ax, kdam_range, lamkin_ssvals[indices[4]])
+
+# lamkin_vals[indices[5]]
+
+# lam_c_val = lamkin_vals[indices[5]][1]
+# kin_c_val = lamkin_vals[indices[5]][2]
+# ω_ab_val = lamkin_vals[indices[5]][3]
 
--- rtc_model/rhlam_coupled/bistab_search/numerical_bistab_search.jl
+++ rtc_model/rhlam_coupled/bistab_search/numerical_bistab_search.jl
@@ -1,4 +1,9 @@
-using JLD2
+using JLD2, GLMakie, InteractiveViz
+
+# decide which model you want to test and set it as variable name
+model = "lamkin_coupled"
+# model = "lam_coupled"
+
 include(joinpath(homedir(), "phd/rtc_model/rhlam_coupled/bistab_search/setup.jl"))
 
 lamkin_vals = []
@@ -13,8 +18,8 @@ for i in lam_c_vals
             test_params[kin_c] = j
             test_params[ω_ab] = wab
             push!(lamkin_vals, (i,j,wab))
-            res_ss = numerical_bistability_analysis(test, test_params, init_rtc, species_rtc, kdam_range, kdam; specie=:rtca)
-            res1_ss = numerical_bistability_analysis(test, test_params, ssvals_rtc, species_rtc, reverse(kdam_range), kdam, specie=:rtca)
+            res_ss = numerical_bistability_analysis(model, test_params, init_rtc, species_rtc, kdam_range, kdam; specie=:rtca)
+            res1_ss = numerical_bistability_analysis(model, test_params, ssvals_rtc, species_rtc, reverse(kdam_range), kdam, specie=:rtca)
             push!(inc, res_ss)
             push!(dec, res1_ss)
         end
@@ -23,11 +28,150 @@ end
 
 @save "/home/hollie_hindley/phd/rtc_model/rhlam_coupled/bistab_search/numerical_bistab_search_smaller.jld2" lamkin_vals inc dec 
 
-# all_res = vcat(inc, dec)
-# inc_lamkin_vals = [("inc",lamkin_vals[i]...) for i in eachindex(lamkin_vals)]
-# dec_lamkin_vals = [("dec",lamkin_vals[i]...) for i in eachindex(lamkin_vals)]
-# all_lamkinvals = vcat(inc_lamkin_vals, dec_lamkin_vals)
-# kdam_inc = fill(kdam_range, length(inc))
-# kdam_dec = fill(reverse(kdam_range), length(dec))
-# kdams = vcat(kdam_inc, kdam_dec)
+
+@load joinpath(homedir(), "phd/rtc_model/rhlam_coupled/bistab_search/numerical_bistab_search_smaller.jld2") lamkin_vals inc dec
+all_res = vcat(inc, dec)
+inc_lamkin_vals = [("inc",lamkin_vals[i]...) for i in eachindex(lamkin_vals)]
+dec_lamkin_vals = [("dec",lamkin_vals[i]...) for i in eachindex(lamkin_vals)]
+all_lamkinvals = vcat(inc_lamkin_vals, dec_lamkin_vals)
+kdam_inc = fill(kdam_range, length(inc))
+kdam_dec = fill(reverse(kdam_range), length(dec))
+kdams = vcat(kdam_inc, kdam_dec)
 # plot([scatter(x=kdams[i], y=all_res[i], name="lam_c, kin_c, wab = $(all_lamkinvals[i])") for i in eachindex(all_res)])
+
+digit = 4
+diff_res = []
+for i in eachindex(inc)
+    if round.(inc[i], digits=digit) != round.(reverse(dec[i]), digits=digit)
+        push!(diff_res, i)
+    end
+end
+diff_res
+
+f = Figure()
+ax=Axis(f[1,1])
+[lines!(ax, kdams[i], all_res[i]) for i in eachindex(all_res)]
+
+f = Figure()
+ax=Axis(f[1,1])
+[lines!(ax, kdam_inc[i], inc[i]) for i in diff_res]
+[lines!(ax, kdam_dec[i], dec[i]) for i in diff_res]
+
+round.(inc[1], digits=6)[1]
+reverse(dec[1])[1]
+
+diff_res
+
+x=9
+f = Figure()
+ax=Axis(f[1,1])
+lines!(ax, kdam_inc[diff_res[x]], inc[diff_res[x]])
+lines!(ax, kdam_dec[diff_res[x]], dec[diff_res[x]])
+
+
+for i in diff_res
+    println(i)
+    f = Figure()
+    ax=Axis(f[1,1])
+    lines!(ax, kdam_inc[i], inc[i])
+    lines!(ax, kdam_dec[i], dec[i])
+    display(f)
+    sleep(2)
+end
+
+inds = [13,26,41,58,67,68,95]
+kdam_range = range(0,60, length=50)
+ind=7
+lamkin_vals[inds[ind]]
+test_params = deepcopy(params_rtc1)
+test_params[lam_c] = lamkin_vals[inds[ind]][1]
+test_params[kin_c] = lamkin_vals[inds[ind]][2]
+test_params[ω_ab] = lamkin_vals[inds[ind]][3]
+res_ss = numerical_bistability_analysis(model, test_params, init_rtc, species_rtc, kdam_range, kdam; specie=:rtca)
+res1_ss = numerical_bistability_analysis(model, test_params, ssvals_rtc, species_rtc, reverse(kdam_range), kdam, specie=:rtca)
+
+f = Figure()
+ax=Axis(f[1,1])
+lines!(ax, kdam_range, res_ss)
+lines!(ax, reverse(kdam_range), res1_ss)
+
+interesting_inds = [26,41,58,67,95]
+
+lamkin_vals[26]
+lamkin_vals[41]
+lamkin_vals[58]
+lamkin_vals[67]
+lamkin_vals[95]
+
+
+
+# 26
+kdam_range26 = range(0,20, length=30)
+test_params = deepcopy(params_rtc1)
+test_params[lam_c] = lamkin_vals[26][1]
+test_params[kin_c] = lamkin_vals[26][2]
+test_params[ω_ab] = lamkin_vals[26][3]
+res_ss26 = numerical_bistability_analysis(model, test_params, init_rtc, species_rtc, kdam_range26, kdam; specie=:rtca)
+res1_ss26 = numerical_bistability_analysis(model, test_params, ssvals_rtc, species_rtc, reverse(kdam_range26), kdam, specie=:rtca)
+
+f26 = Figure()
+ax26=Axis(f26[1,1])
+lines!(ax26, kdam_range26, res_ss26)
+lines!(ax26, reverse(kdam_range26), res1_ss26)
+
+# 41
+kdam_range41 = range(0,50, length=30)
+test_params = deepcopy(params_rtc1)
+test_params[lam_c] = lamkin_vals[41][1]
+test_params[kin_c] = lamkin_vals[41][2]
+test_params[ω_ab] = lamkin_vals[41][3]
+res_ss41 = numerical_bistability_analysis(model, test_params, init_rtc, species_rtc, kdam_range41, kdam; specie=:rtca)
+res1_ss41 = numerical_bistability_analysis(model, test_params, ssvals_rtc, species_rtc, reverse(kdam_range41), kdam, specie=:rtca)
+
+f41 = Figure()
+ax41=Axis(f41[1,1])
+lines!(ax41, kdam_range41, res_ss41)
+lines!(ax41, reverse(kdam_range41), res1_ss41)
+
+# 58
+kdam_range58 = range(0,20, length=30)
+test_params = deepcopy(params_rtc1)
+test_params[lam_c] = lamkin_vals[58][1]
+test_params[kin_c] = lamkin_vals[58][2]
+test_params[ω_ab] = lamkin_vals[58][3]
+res_ss58 = numerical_bistability_analysis(model, test_params, init_rtc, species_rtc, kdam_range, kdam; specie=:rtca)
+res1_ss58 = numerical_bistability_analysis(model, test_params, ssvals_rtc, species_rtc, reverse(kdam_range), kdam, specie=:rtca)
+
+f58 = Figure()
+ax58=Axis(f58[1,1])
+lines!(ax58, kdam_range58, res_ss58)
+lines!(ax58, reverse(kdam_range58), res1_ss58)
+
+# 67
+kdam_range67 = range(0,20, length=30)
+test_params = deepcopy(params_rtc1)
+test_params[lam_c] = lamkin_vals[67][1]
+test_params[kin_c] = lamkin_vals[67][2]
+test_params[ω_ab] = lamkin_vals[67][3]
+res_ss67 = numerical_bistability_analysis(model, test_params, init_rtc, species_rtc, kdam_range, kdam; specie=:rtca)
+res1_ss67 = numerical_bistability_analysis(model, test_params, ssvals_rtc, species_rtc, reverse(kdam_range), kdam, specie=:rtca)
+
+f67 = Figure()
+ax67=Axis(f67[1,1])
+lines!(ax67, kdam_range67, res_ss67)
+lines!(ax67, reverse(kdam_range67), res1_ss67)
+
+# 95
+kdam_range95 = range(0,20, length=30)
+test_params = deepcopy(params_rtc1)
+test_params[lam_c] = lamkin_vals[95][1]
+test_params[kin_c] = lamkin_vals[95][2]
+test_params[ω_ab] = lamkin_vals[95][3]
+res_ss95 = numerical_bistability_analysis(model, test_params, init_rtc, species_rtc, kdam_range, kdam; specie=:rtca)
+res1_ss95 = numerical_bistability_analysis(model, test_params, ssvals_rtc, species_rtc, reverse(kdam_range), kdam, specie=:rtca)
+
+f95 = Figure()
+ax95=Axis(f95[1,1])
+lines!(ax95, kdam_range95, res_ss95)
+lines!(ax95, reverse(kdam_range95), res1_ss95)
+
