05b168235d1448e4948fbb98d5f5beed3e02cd02
hhindley <s2257179@ed.ac.uk>
2024-09-24 10:32:55 +0100

    kdam vary with just lam

--- rtc_model/rhlam_coupled/bifurcationkit_bistab.jl
+++ rtc_model/rhlam_coupled/bifurcationkit_bistab.jl
@@ -31,7 +31,7 @@ p_conc = plot(scatter(x=df.kdam, y=df.rtca, name="RtcA"),
 
 
 
-prob_bf = ODEProblem(test, ssvals_rtc, tspan, params_rtc1; jac=true)
+prob_bf = ODEProblem(lamkin_coupled, ssvals_rtc_test, tspan, test_params; jac=true)
 odefun = prob_bf.f
 F = (u,p) -> odefun(u,p,0)
 J = (u,p) -> odefun.jac(u,p,0)
@@ -42,7 +42,7 @@ id_kdam = indexof(0.0, par_tm)
 
 prob = BifurcationProblem(F, prob_bf.u0, (par_tm), (@lens _[id_kdam]); J=J,
 record_from_solution = (x, p) -> (rm_a = x[1], rtca = x[2], rm_b = x[3], rtcb = x[4], rm_r = x[5], rtcr = x[6], rh = x[7], rd = x[8], rt = x[9]),)
-opts_br = ContinuationPar(p_min = 0., p_max = 50., ds = 1e-4, #a=0.1,
+opts_br = ContinuationPar(p_min = 0., p_max = 200., ds = 1e-4, #a=0.1,
 dsmax = 0.0005, dsmin = 1e-6, # 0.15
 # options to detect bifurcations
 detect_bifurcation = 3, n_inversion = 2, max_bisection_steps = 50, #3,2,10
@@ -63,6 +63,9 @@ bf = bf_point_df(br)
 kdam1 = findall(x->x==bf.kdam[1],df1.kdam)[1]
 kdam2 = findall(x->x==bf.kdam[2],df1.kdam)[1]
 kdam3 = findall(x->x==bf.kdam[3],df1.kdam)[1]
+kdam4 = findall(x->x==bf.kdam[4],df1.kdam)[1]
+kdam5 = findall(x->x==bf.kdam[5],df1.kdam)[1]
+kdam6 = findall(x->x==bf.kdam[6],df1.kdam)[1]
 
 f = Figure()
 ax = Axis(f[1,1], xlabel="kdam", ylabel="rtca")
@@ -70,6 +73,9 @@ lines!(ax, df1.kdam, df1.rtca)
 scatter!(ax, [df1.kdam[kdam1]], [df1.rtca[kdam1]])
 scatter!(ax, [df1.kdam[kdam2]], [df1.rtca[kdam2]])
 scatter!(ax, [df1.kdam[kdam3]], [df1.rtca[kdam3]])
+scatter!(ax, [df1.kdam[kdam4]], [df1.rtca[kdam4]])
+scatter!(ax, [df1.kdam[kdam5]], [df1.rtca[kdam5]])
+scatter!(ax, [df1.kdam[kdam6]], [df1.rtca[kdam6]])
 
 # plot(scatter(x=df1.kdam, y=df1.rtca, name="RtcA"), 
 #       Layout(xaxis_title="Damage rate (min<sup>-1</sup>)",yaxis_title="Concentration μM"))
--- rtc_model/rhlam_coupled/bistab_search/kdam_vary_search.jl
+++ rtc_model/rhlam_coupled/bistab_search/kdam_vary_search.jl
@@ -10,6 +10,7 @@ kdam_range = range(0,100,length=20)
 
 lamkin_ssvals = []
 lamkin_vals = []
+
 global iteration_num = 0
 println("starting loop")
 for i in lam_c_vals
@@ -31,47 +32,168 @@ end
 
 println("saving variables")
 @save joinpath(homedir(), "phd/rtc_model/rhlam_coupled/bistab_search/kdam_vary_search.jld2") lamkin_ssvals lamkin_vals
-println("finished at $(Dates.now())")
+# println("finished at $(Dates.now())")
 
-# @load joinpath(homedir(),"phd/rtc_model/rhlam_coupled/bistab_search/kdam_vary_search.jld2") lamkin_ssvals lamkin_vals
-
-
-# plot([scatter(x=kdam_range, y=lamkin_ssvals[i], name="lam_c, kin_c, wab = $(lamkin_vals[i])") for i in eachindex(lamkin_vals)], Layout(xaxis_title="kdam", yaxis_title="rtca"))
+@load joinpath(homedir(),"phd/rtc_model/rhlam_coupled/bistab_search/kdam_vary_search.jld2") lamkin_ssvals lamkin_vals
 
 
+# # plot([scatter(x=kdam_range, y=lamkin_ssvals[i], name="lam_c, kin_c, wab = $(lamkin_vals[i])") for i in eachindex(lamkin_vals)], Layout(xaxis_title="kdam", yaxis_title="rtca"))
 
 
-# function bistab_check(ssvals)
-#     m, m_ind = findmax(ssvals)
-#     for i in ssvals[m_ind:end]
-#         if ((i-m)/m*100) < -50
-#             return true
-#         end
-#     end
-#     return false
-# end
-# indices=[]
-# for i in eachindex(lamkin_ssvals)
-#     if bistab_check(lamkin_ssvals[i])
-#         push!(indices, i)
-#     end
-# end
-# indices
 
-# f = Figure()
-# ax = Axis(f[1, 1])
-# [ilines!(ax, kdam_range, lamkin_ssvals[i]) for i in indices]
 
-# lamkin_vals[indices]
+function bistab_check(ssvals)
+    m, m_ind = findmax(ssvals)
+    for i in ssvals[m_ind:end]
+        if ((i-m)/m*100) < -50
+            return true
+        end
+    end
+    return false
+end
+indices=[]
+for i in eachindex(lamkin_ssvals)
+    if bistab_check(lamkin_ssvals[i])
+        push!(indices, i)
+    end
+end
+indices
 
-# lamkin_ssvals[indices[4]]
-# f = Figure()
-# ax = Axis(f[1, 1])
-# lines!(ax, kdam_range, lamkin_ssvals[indices[4]])
+f = Figure()
+ax = Axis(f[1, 1])
+[ilines!(ax, kdam_range, lamkin_ssvals[i]) for i in indices]
 
-# lamkin_vals[indices[5]]
+bigger_res = []
+for i in eachindex(lamkin_ssvals)
+    if maximum(lamkin_ssvals[i]) > 0.001 && maximum(lamkin_ssvals[i]) < 10
+        push!(bigger_res, i)
+    end
+end
+bigger_res
+f = Figure()
+ax = Axis(f[1, 1])
+[ilines!(ax, kdam_range, lamkin_ssvals[i]) for i in bigger_res]
 
-# lam_c_val = lamkin_vals[indices[5]][1]
-# kin_c_val = lamkin_vals[indices[5]][2]
-# ω_ab_val = lamkin_vals[indices[5]][3]
 
+lamkin_vals[bigger_res]
+
+lamkin_ssvals[indices[4]]
+f = Figure()
+ax = Axis(f[1, 1])
+lines!(ax, kdam_range, lamkin_ssvals[indices[1]])
+
+lamkin_vals[indices[5]]
+
+lam_c_val = lamkin_vals[indices[1]][1]
+kin_c_val = lamkin_vals[indices[1]][2]
+ω_ab_val = lamkin_vals[indices[1]][3]
+
+kdam_range1 = range(0,200,length=1000)
+
+test_params = deepcopy(params_rtc1)
+test_params[lam_c] = lam_c_val
+test_params[kin_c] = kin_c_val
+test_params[ω_ab] = ω_ab_val
+ssvals_rtc_test = steady_states(lamkin_coupled, init_rtc, test_params)
+res = var_param(lamkin_coupled, kdam, test_params, kdam_range1, ssvals_rtc_test)
+
+f = Figure()
+ax = Axis(f[1, 1])
+lines!(ax, kdam_range1, res.rtca)
+
+
+
+
+
+
+
+
+lam_c_vals = 10 .^range(log10(1e-8),log10(0.1), length=10) #8e-7
+kin_c_vals = 10 .^range(log10(1e-6),log10(0.1), length=10) #1.5e-5
+wab_vals = 10 .^range(log10(1e-6),log10(0.1), length=10) #1.3e-5
+kdam_range = range(0,100,length=20)
+
+lamkin_ssvals1 = []
+lamkin_vals1 = []
+times_df=[]
+global iteration_num = 0
+println("starting loop")
+for i in lam_c_vals
+    for j in kin_c_vals
+        for wab in wab_vals
+            global iteration_num += 1
+            println("iteration: $iteration_num")
+            test_params = deepcopy(params_rtc1)
+            test_params[lam_c] = i
+            test_params[kin_c] = j
+            test_params[ω_ab] = wab
+            test_params[kdam] = 0
+            push!(lamkin_vals1, (i,j,wab))
+            solu = sol(lamkin_coupled, init_rtc, tspan, test_params)
+            df = create_solu_df(solu, species_rtc)
+            # ssvals_rtc_test = steady_states(lamkin_coupled, init_rtc, test_params)
+            # res = var_param(lamkin_coupled, kdam, test_params, kdam_range, ssvals_rtc_test)
+            push!(lamkin_ssvals1, df.rtca)
+            push!(times_df, df.time)
+        end
+    end
+end
+
+times_df[1]
+lamkin_ssvals1[1]
+
+f = Figure()
+ax = Axis(f[1, 1], xscale=log10)
+[lines!(ax, times_df[i], lamkin_ssvals1[i]) for i in eachindex(lamkin_ssvals1)]
+
+lamkin_ssvals1
+
+good_range = []
+for i in eachindex(lamkin_ssvals1)
+    if lamkin_ssvals1[i][end] < 0.5 && lamkin_ssvals1[i][end] > 0.001
+        push!(good_range, i)
+    end
+end
+good_range
+
+f = Figure()
+ax = Axis(f[1, 1], xscale=log10)
+[lines!(ax, times_df[i], lamkin_ssvals1[i]) for i in (good_range)]
+
+
+
+lams=[lamkin_vals1[good_range][i][1] for i in eachindex(lamkin_vals1[good_range])]
+minimum(lams)
+maximum(lams)
+
+kins=[lamkin_vals1[good_range][i][2] for i in eachindex(lamkin_vals1[good_range])]
+minimum(kins)
+maximum(kins)
+
+wabs=[lamkin_vals1[good_range][i][3] for i in eachindex(lamkin_vals1[good_range])]
+minimum(wabs)
+maximum(wabs)
+
+
+lam_c_range = range(minimum(lams), maximum(lams), length=20)
+wab_range = range(minimum(wabs), maximum(wabs), length=20)
+kdam_range = range(0,200,length=200)
+
+lam_ssvals = []
+lam_vals = []
+
+global iteration_num = 0
+println("starting loop")
+for i in lam_c_vals
+    for wab in wab_vals
+        global iteration_num += 1
+        println("iteration: $iteration_num")
+        test_params = deepcopy(params_rtc1)
+        test_params[lam_c] = i
+        test_params[ω_ab] = wab
+        push!(lam_vals, (i,wab))
+        ssvals_rtc_test = steady_states(lam_coupled, init_rtc, test_params)
+        res = var_param(lam_coupled, kdam, test_params, kdam_range, ssvals_rtc_test)
+        push!(lam_ssvals, res.rtca)
+    end
+end
